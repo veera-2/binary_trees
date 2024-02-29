0ea387f54001dba65cabe815f4506c8debaff530
veera-2 <naveera.nh@gmail.com>
2024-02-29 16:50:05 +0100

    insert right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,22 +18,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
-	new_node = malloc(sizeof(binary_tree_t));
 
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = parent->left;
 
 	if (parent->left != NULL)
 	{
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
