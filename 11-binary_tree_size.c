003cd2a41e6f0f89c40b4c9e5b085ca62bb3ae5e
Rahmat <rahmatbola3@gmail.com>
2024-02-29 12:09:55 -0800

    correction of binarySize

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,14 +1,19 @@
-#include <stddef.h> // For size_t
+#include <stddef.h> /* For size_t*/
 #include "binary_trees.h"
+/**
+ * binary_tree_size - size of the binary tree
+ * @tree: pointer to node
+ * Return: size of tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
 		/* Count the current node plus the sizes of its left and right subtrees*/
-		return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+		return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 	}
 }
