e1713fab234c4b1b5db0a4e00e2a143d4eb84a73
veera-2 <naveera.nh@gmail.com>
2024-02-29 17:35:31 +0100

    check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,17 +9,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-	{
+	if (node == NULL || node->parent != NULL)
 		return (0);
-	}
 
-	if (node->left == NULL && node->right == NULL)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (1);
 }
